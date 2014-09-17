2dcbb2a3ef26fb240b45e7f32ef8b267aeb22b7e
u353002 <u353002@L211396.post.bpgnet.net>
2014-09-17 17:21:54 +0200

    selenium setup

---
+++ serv-war/services/src/test/java/be/post/webdriver/CspLogin.java
@@ -0,0 +1,49 @@
+package be.post.webdriver;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CspLogin {
+    public static void main(String[] args) throws Exception {
+        // The Firefox driver supports javascript 
+    	WebDriver driver = new FirefoxDriver();
+               
+        
+        // Go to the Google Suggest home page        
+        driver.get("http://csp-dv2.netpost/csp/indexPage.do");
+        
+        // Enter the query string "Cheese"
+        WebElement query = driver.findElement(By.name("j_username"));       
+        query.sendKeys("u353002");
+        
+        WebElement queryPwd = driver.findElement(By.name("j_password"));
+        queryPwd.sendKeys("422");
+        
+        driver.findElement(By.xpath("//input[@value='OK']")).click();
+
+        // Sleep until the div we want is visible or 5 seconds is over
+        long end = System.currentTimeMillis() + 5000;
+        while (System.currentTimeMillis() < end) {
+            WebElement resultsDiv = driver.findElement(By.xpath("//div[@id='tasksArea']"));
+
+            // If results have been returned, the results are displayed in a drop down.
+            if (resultsDiv.isDisplayed()) {
+              break;
+            }
+        }
+
+        // And now list the suggestions
+        List<WebElement> allSuggestions = driver.findElements(By.xpath("//a"));
+        
+        for (WebElement suggestion : allSuggestions) {
+            System.out.println(suggestion.getText());
+        }
+
+        driver.quit();
+    }
+}
